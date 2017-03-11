0ccd8d8f1af20925594db91c8e867f6a95c8fdf2
Michael Costanzo <mvcostanzo@gmail.com>
2017-03-11 15:58:33 -0500

    First Pass

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,39 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if !(validateInput(input)){
+        return false;
+    }
+    else{
+        attempt+=1;
+    }
 }
 
 //implement new functions here
+//implement new functions here
+function setHiddenFields(){
+    answer = Math.floor(Math.random()).toString();
+    while (answer.length < 4){
+        answer = "0" + answer;
+    }
+    attempt = 0;
+    return answer;
+
+
+function setMessage(message){
+    document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(guess){
+    if (guess.length == 4){
+        return true;
+    }
+    else{
+        setMessage('Guesses must be exactly 4 characters long.');
+        return false;
+    }
+}
+
+function getResults(input){
+    
+}
